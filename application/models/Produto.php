5d5a9488ade4d8c90995bf18b5f3d306d1994f75
Vinícius Gorgonho <vinicius2pi@gmail.com>
2023-10-11 21:25:31 -0400

    Criei o model do Produto

--- application/models/Produto.php
+++ application/models/Produto.php
@@ -1,5 +1,19 @@
 <?php 
-
 class Produto{
-    
+    private $codigo;
+    private $nome;
+    private $marca;
+    private $preco;
+public function __construct($nome, $marca,$preco){
+       $this->nome = $nome;
+       $this->marca = $marca;
+       $this->preco = $preco;
+    }
+    public function setCodigo($codigo){
+        $this->codigo = $codigo;
+    }
+    public function getCodigo(){
+        return $this->codigo;
+    }
+    public function getNome(){return $this->nome;}
 }
