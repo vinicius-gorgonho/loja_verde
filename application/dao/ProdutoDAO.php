23c73598362d277c4263f782d9a4e28b70a47273
Vinícius Gorgonho <vinicius2pi@gmail.com>
2023-10-11 21:42:00 -0400

    Criei o produto DAO

---
+++ application/dao/ProdutoDAO.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProdutoDAO{
+
+    // Create (C)
+    public function salvar($produto){}
+    public function pegaTodos(){}
+    // Retrieve (R)
+    public function pegaPorId($id){}
+    // Update (U)
+    public function atualizar($produto){}
+    // Delete (D)
+    public function apagar($id){}
+
+}
+
+
+?>
