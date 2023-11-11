4788547a3a8d3a581007bfbb6e0fbfaccbde0e46
higor.campos <higor.fcampos2001@gmail.com>
2023-11-10 20:12:00 -0400

    Alteração em ProdutoDAO

--- application/dao/ProdutoDAO.php
+++ application/dao/ProdutoDAO.php
@@ -3,16 +3,49 @@
 class ProdutoDAO{
    
     // Create (C)
+
     public function salvar($produto){}
-    public function pegaTodos(){}
+
+    public function findAll()
+    $conexao = new Conexao();
+
+    $conn = $conexao ->getConexao();
+    $SQL = "SELECT * FROM produtos";
+    $result = $conn -> query($SQL);
+    $produtos = [];
+    while($row = $result->fetch_assoc()){
+        $produto = new Produto($row["nome"],$row["marca"],$row["preco"]);
+        $produto ->setCodigo($row["codigo"]);
+        array_push($produtos, $produto);
+    }
+    return $produtos;
+}
+
+
+
     // Retrieve (R)
-    public function pegaPorId($id){}
+
+    public function findById($id){
+        $conxao = new Conexao();
+        $conn = $conexao ->getconexao();
+        $SQL = "SELECT * FROM produtos WHERE codigo =".$id;
+        $result = conn ->query($SQL);
+        $row = $result -> fetch_assoc();
+        $produto = new Produto($row["nome"],$row["marca"],$row["preco"]);
+        return $produto;
+    }
+
+
     // Update (U)
+
     public function atualizar($produto){}
+
+
     // Delete (D)
+    
     public function apagar($id){}
 
-}
+
 
 
 ?>
